a0665efa258dd1117fe7e327172126b41c6cb1fe
arian reza <arian.reza.setiawan@gmail.com>
2024-11-12 20:40:40 +0700

    add direnv

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+
+  imports = [
+  	./cli.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "jnkk";
