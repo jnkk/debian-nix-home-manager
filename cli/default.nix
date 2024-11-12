5e934ce1ac8ba20afcfecf08e3da42c996e643ec
arian reza <arian.reza.setiawan@gmail.com>
2024-11-12 21:49:24 +0700

    testing nushell

--- cli/default.nix
+++ cli/default.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }:
 {
 
-	programs.zsh = {
-		enable = true;
-		
-
-		oh-my-zsh = {
-			enable = true;
-			plugins = [ eza bat ];
-			theme = "guezwhoz";
-		};
-	};
+	# programs.zsh = {
+	# 	enable = true;
+	# 	enableCompletion = true;
+	# 	oh-my-zsh = {
+	# 		enable = true;
+	# 		plugins = ["eza" "direnv" ];
+	# 		theme = "";
+	# 	};
+	# };
 	
+	programs.nushell = {
+		enable = true;
+	};
+
+
 }
