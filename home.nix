974f9c808554b42f4fdf394a1f1fa5a4e1056646
Arian Reza <arian.reza.setiawan@gmail.com>
2024-12-01 09:52:53 +0700

    rust things, bruno

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -31,7 +31,8 @@
   home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    vscodium fastfetch bat 
+    vscodium fastfetch bat lsd rustup rustc rustscan rusty-bash
+    bruno bruno-cli helix
     
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
