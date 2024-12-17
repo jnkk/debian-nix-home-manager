97a567a2ef17f9a1762bf946f85e5c3b9eef7ac1
Arian Reza <arian.reza.setiawan@gmail.com>
2024-12-18 00:20:47 +0700

    remove nushell

--- home.nix
+++ home.nix
@@ -38,7 +38,7 @@
     go redis
     pgadmin4-desktopmode  
     neovim devenv
-    emacs nushell nurl
+    emacs nurl
      
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
