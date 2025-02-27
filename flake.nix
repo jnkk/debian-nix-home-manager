69b2a67241bf5dd52a9e58d2969f2386424fc2dd
Arian Reza <arian.reza.setiawan@gmail.com>
2025-02-27 13:25:30 +0700

    change back to unstable

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    # nix-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";        
     # roc.url = "github:roc-lang/roc";
     # ghostty = {
     	# url = "github:ghostty-org/ghostty";
